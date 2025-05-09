ec77320842656147494112aea9ff5164ced5ab31
ERKAN TURGUT <159196857+Erkan3034@users.noreply.github.com>
2025-05-09 23:26:31 +0300

    Add files via upload

---
+++ App/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'music-player-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://www.youtube.com/iframe_api',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+}); 
